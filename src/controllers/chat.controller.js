4093482f32f1bfec0134b44d90cae5ebe198a407
Barsoum Adly <barsoumadly@gmail.com>
2025-08-27 20:37:32 +0300

    chat controller

--- src/controllers/chat.controller.js
+++ src/controllers/chat.controller.js
@@ -4,10 +4,10 @@ const getStreamToken = async function (request, response) {
   try {
     const token = generateStreamToken(req.user.id);
 
-    res.status(200).json({ success: true, token });
+    response.status(200).json({ success: true, token });
   } catch (error) {
     console.log("Error in getStreamToken controller:", error.message);
-    res.status(500).json({ message: "Internal Server Error" });
+    response.status(500).json({ message: "Internal Server Error" });
   }
 };
 
